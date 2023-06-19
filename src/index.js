b5e370922dd5ee77ff42c98ef77469b83eb1d20b
Alphabeee <bob20031002@gmail.com>
2023-06-19 15:45:31 +0800

    fix

--- src/index.js
+++ src/index.js
@@ -6,13 +6,13 @@ if (require("electron-squirrel-startup")) {
   app.quit();
 }
 const basex = [0, 1400, 0, 250, 500, 750, 1000, 1250, 1500, 0, 1300],
-  basey = [200, 500, 0, 700, 0, 700, 0, 700, 0, 0, 0],
+  basey = [200, 500, 40, 700, 40, 700, 40, 700, 40, 40, 40],
   dirx = [1, -1, 0, 0, 0, 0, 0, 0, 0, 1, -1],
   diry = [0, 0, 1, -1, 1, -1, 1, -1, 1, 1, 1],
   tim = [2500, 2500, 4500, 4500, 4500, 4500, 4500, 4500, 4500, 4500],
-  tim2 = [150, 150, 100, 100, 100, 100, 100, 100, 100, 100, 100],
+  tim2 = [150, 150, 150, 150, 150, 150, 150, 150, 300, 200, 200],
   k = [15, 15, 10, 10, 10, 10, 10, 10, 10],
-  r = [100, 100, 100, 100, 100, 100, 100, 100, 100, 70, 70];
+  r = [100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100];
 const createWindow = (i, j) => {
   // Create the browser window.
   const Window1 = new BrowserWindow({
@@ -34,8 +34,8 @@ const createWindow = (i, j) => {
 };
 const circle = (i, j) => {
   // Create the browser window.
-  let X = Math.floor(300 * Math.cos((i * Math.PI) / 180) + 650),
-    Y = Math.floor(300 * Math.sin((i * Math.PI) / 180) + 300);
+  let X = Math.floor(270 * Math.cos((i * Math.PI) / 180) + 650),
+    Y = Math.floor(270 * Math.sin((i * Math.PI) / 180) + 320);
   //console.log(`x=${X},y=${Y}`);
 
   const Window1 = new BrowserWindow({
@@ -58,8 +58,8 @@ const circle = (i, j) => {
 const SIN = (i, j) => {
   // Create the browser window.
   let X = i,
-    Y = Math.floor(50 * Math.sin((i * Math.PI) / 180) + 700);
-  if (Y > 700) Y = 1400 - Y;
+    Y = Math.floor(70 * Math.sin((i * Math.PI) / 180) + 650);
+  if (Y > 650) Y = 1300 - Y;
   //console.log(`x=${X},y=${Y}`);
 
   const Window1 = new BrowserWindow({
@@ -73,16 +73,16 @@ const SIN = (i, j) => {
   });
 
   // and load the index.html of the app.
-  const val = Math.floor(Math.random() * 3);
-  if (val == 0) Window1.loadFile(path.join(__dirname, "index.html"));
-  else if (val == 1) Window1.loadFile(path.join(__dirname, "index2.html"));
-  else Window1.loadFile(path.join(__dirname, "index3.html"));
+  Window1.loadFile(path.join(__dirname, "index.html"));
   return Window1;
 };
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
 app.on("ready", async () => {
+  for (let i = 0; i < 10000; i++) {
+    for (let j = 0; j < 10000; j++);
+  }
   for (let j = 0; j < 8; j++)
     for (let i = 0; i < k[j]; i++) {
       const w = createWindow(j, i);
@@ -103,21 +103,24 @@ app.on("ready", async () => {
 
     await new Promise((r) => setTimeout(r, tim2[9]));
   }
-
-  for (let i = 0; i <= 360; i += 5) {
+  for (let i = 0; i <= 360; i += 6) {
     const w = circle(i, 0);
 
     setTimeout(() => {
       w.close();
-    }, 7500);
+    }, 7300);
 
-    await new Promise((r) => setTimeout(r, 30));
+    await new Promise((r) => setTimeout(r, 50));
   }
-  for (let i = 0; i <= 1400; i += 15) {
+  let ans = 0;
+  for (let i = 0; i < 300000; i++) {
+    for (let j = 0; j < 10000; j++) ans++;
+  }
+  for (let i = 0; i <= 1530; i += 18) {
     const w = SIN(i, 0);
     setTimeout(() => {
       w.close();
-    }, 3500);
+    }, 2000);
     await new Promise((r) => setTimeout(r, 50));
   }
 });
